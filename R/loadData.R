b64502e0311698cd5e491e68e948984a61373a8f
Cameron Roach <camroach87@gmail.com>
2016-11-26 17:56:50 +1100

    Added load zones as input for load data

--- R/loadData.R
+++ R/loadData.R
@@ -1,63 +1,52 @@
-# Description: Generates forecasts for GEFCom2017-D
-#
-# Author: Cameron Roach
-
-rm(list=ls())
-
-require(dplyr)
-require(tidyr)
-require(readxl)
-require(lubridate)
-require(ggplot2)
-require(plotly)
-require(caret)
-require(myhelpr)
-
-
-#### Load data ========================
-
-holidays <- read.csv("./data/holidays/holidays.csv") %>% 
-  mutate(Date = mdy(Date))
-
-load_zones_ma <- c("SEMASS", "WCMASS", "NEMASSBOST")
-load_zones <- c("ME", "NH", "VT", "CT", "RI", load_zones_ma)
-
-
-smd <- NULL
-files <- list.files("./data/smd")
-for (iF in files) {
-  cat("Reading file", iF, "...\n")
-  file_name <- file.path("./data/smd", iF)
+#' Load SMD data
+#' 
+#' Loadas data for GEFCom2017-D
+#' 
+#' @param root_dir Path to the root directory.
+#' @param load_zones Electricity load zones.
+#'
+#' 
+#' @return smd data_frame containing raw data and calendar variables
+#' @export
+#' 
+#' @examples
+#' 
+#' @author Cameron Roach
+load_smd_data <- function(root_dir = ".", load_zones) {
+  # TODO: Why are there NAs appearing in ts?
+  #
+  # TODO: Use holidays data frame to add dummy variable for holiday in smd data 
+  # frame
   
-  for (iS in load_zones) {
-    tmp <- read_excel(file_name, sheet = iS) %>% 
-      mutate(Zone = iS) %>% 
-      select(Date, Hour, Zone, Demand = DEMAND, DryBulb, DewPnt)
+  holidays <- read.csv(file.path(root_dir, "data/holidays/holidays.csv")) %>% 
+    mutate(Date = mdy(Date))
+  
+  
+  smd <- NULL
+  files <- list.files(file.path(root_dir, "data/smd"))
+  for (iF in files) {
+    cat("Reading file", iF, "...\n")
+    file_name <- file.path(root_dir, "data/smd", iF)
     
-    smd <- bind_rows(smd, tmp)
+    for (iS in load_zones) {
+      tmp <- read_excel(file_name, sheet = iS) %>% 
+        mutate(Zone = iS) %>% 
+        select(Date, Hour, Zone, Demand = DEMAND, DryBulb, DewPnt)
+      
+      smd <- bind_rows(smd, tmp)
+    }
   }
+  
+  # Add extra variables
+  smd <- smd %>% 
+    mutate(Date = if_else(Hour == 24, Date + days(1), Date),
+           Hour = if_else(Hour == 24, 0, Hour),
+           ts = ymd_h(paste(Date, Hour)),
+           Year = year(ts),
+           Month = month(ts, label = TRUE),
+           DoW = wday(ts, label = TRUE),
+           Weekend = ifelse(DoW %in% c("Sat", "Sun"), TRUE, FALSE),
+           DryDewDiff = DryBulb - DewPnt)
+  
+  return(smd)
 }
-
-# Add extra variables
-smd <- smd %>% 
-  mutate(Date = if_else(Hour == 24, Date + days(1), Date),
-         Hour = if_else(Hour == 24, 0, Hour),
-         ts = ymd_h(paste(Date, Hour)),
-         Year = year(ts),
-         Month = month(ts, label = TRUE),
-         DoW = wday(ts, label = TRUE),
-         Weekend = ifelse(DoW %in% c("Sat", "Sun"), TRUE, FALSE),
-         DryDewDiff = DryBulb - DewPnt)
-
-
-
-# TODO: Why are there NAs appearing in ts?
-#
-# TODO: Use holidays data frame to add dummy variable for holiday in smd data 
-# frame
-#
-# TODO: Add day of week variable
-#
-# TODO: Check if some holidays have a bigger impact on energy than others -
-# check boxplot
-
