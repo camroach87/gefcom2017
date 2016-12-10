35b02b455391defeaf0a59568b8873d2961b1abd
Cameron Roach <camroach87@gmail.com>
2016-12-10 13:31:00 +1100

    Added new function to create dummy variables. Removed dummy variable creation from load_data function.

--- gefcom2017/R/load-data.R
+++ gefcom2017/R/load-data.R
@@ -37,25 +37,6 @@ load_smd_data <- function(load_zones, root_dir = ".") {
       }
     }
 
-    # Add extra variables
-    # Note that factors are not ordered as this can adversely impact model fitting time in caret.
-    smd <- smd %>%
-      mutate(Date = if_else(Hour == 24, Date + days(1), Date),
-             Period = factor(smd$Hour, levels = 1:24, ordered = FALSE),
-             Hour = if_else(Hour == 24, 0, Hour),
-             ts = ymd_h(paste(Date, Hour)),
-             Year = year(ts),
-             Month = factor(month(ts, label = TRUE), ordered = FALSE),
-             DoW = factor(wday(ts, label = TRUE), ordered = FALSE),
-             DoY = yday(ts),
-             Weekend = ifelse(DoW %in% c("Sat", "Sun"), TRUE, FALSE),
-             DryDewDiff = DryBulb - DewPnt)
-
-    # Add holidays
-    smd <- left_join(smd, holidays) %>%
-      mutate(Holiday = if_else(is.na(Holiday), "NH", Holiday),
-             Holiday_flag = if_else(Holiday == "NH", FALSE, TRUE))
-
     # cache smd data frame for speedy loading
     dir.create(file.path(root_dir, "cache"),
                showWarnings = FALSE)
