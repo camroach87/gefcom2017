7e612c32c5dacc5477c2d9b8b71bb8581efaa9cf
Cameron Roach <camroach87@gmail.com>
2017-08-16 18:04:54 +1000

    Add sample similar day function. Not complete yet.

--- R/sample-similar-day.R
+++ R/sample-similar-day.R
@@ -49,3 +49,52 @@ sample_similar_day <- function(fcst_dates, resid_dates) {
 
   return(fcst_dates)
 }
+
+
+
+#' Sample sequence of similar days
+#'
+#' Takes a vector of dates as inputs and randomly samples a random number of
+#' from the residual data frame. Similar dates can be defined by day of the week
+#' or working/non-working days.
+#'
+#'
+#' @param fcst_dates data frame containing forecasts dates. These are the dates
+#'   we wish to find similar dates for.
+#' @param resid_dates data frame of residual date info. Must contain Date and
+#'   Holiday_flag columns. Only unique values should be included.
+#'
+#' @return The forecast data frame with a new residuals column.
+#' @export
+#'
+#' @author Cameron Roach
+sample_sequence_similar_days <- function(fcst_dates, resid_dates) {
+  root_dir <- system.file("extdata", package = "gefcom2017")
+  holidays <- read.csv(file.path(root_dir, "holidays/holidays.csv"),
+                       stringsAsFactors = FALSE) %>%
+    mutate(Date = mdy(Date))
+
+  fcst_dates = data_frame(Date = fcst_dates) %>%
+    left_join(holidays) %>%
+    mutate(Holiday = if_else(is.na(Holiday), "NH", Holiday),
+           Holiday_flag = if_else(Holiday == "NH", FALSE, TRUE))
+
+  for (iD in 1:length(fcst_dates$Date)) {
+    resid_like_days <- resid_dates %>%
+      filter(wday(Date) == wday(fcst_dates$Date[iD]),
+             Holiday_flag == fcst_dates$Holiday_flag[iD])
+
+    if (iD == 1) {
+      resid_date_samples <- sample(resid_like_days$Date, 1)
+    } else {
+      resid_date_samples <- c(resid_date_samples,
+                              sample(resid_like_days$Date, 1))
+    }
+  }
+
+  fcst_dates <- fcst_dates %>%
+    mutate(Resid_date = resid_date_samples) %>%
+    select(Date, Resid_date)
+
+  return(fcst_dates)
+}
