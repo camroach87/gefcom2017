914ca6b319afbc7af95ee8117d1e909a3b8ac498
Cameron Roach <camroach87@gmail.com>
2017-08-27 15:34:48 +1000

    Add date drift argument to sample_sequence_similar_days

--- R/sample-similar-day.R
+++ R/sample-similar-day.R
@@ -53,26 +53,29 @@ sample_similar_day <- function(fcst_dates, resid_dates) {
 
 
 #' Sample sequence of similar days
-#' 
-#' Takes a vector of dates as inputs and randomly samples a random number of 
+#'
+#' Takes a vector of dates as inputs and randomly samples a random number of
 #' from the residual data frame. Similar dates can be defined by day of the week
 #' or working/non-working days.
-#' 
-#' 
+#'
+#'
 #' @param fcst_dates vector containing forecasts dates. These are the dates we
 #'   wish to find similar dates for.
 #' @param resid_dates data frame of residual date info. Must contain Date
 #'   column. Only unique values should be included.
 #' @param min_block_length integer giving minimum block length in days.
 #' @param max_block_length integer giving maximum block length in days.
-#' 
+#' @param date_drift integer specifying number of days that block start length
+#'   can differ from current day of year.
+#'
 #' @return The forecast data frame with a new residuals column.
 #' @export
-#' 
+#'
 #' @author Cameron Roach
 sample_sequence_similar_days <- function(fcst_dates, resid_dates,
                                          min_block_length = 6,
-                                         max_block_length = 14) {
+                                         max_block_length = 14,
+                                         date_drift = 7) {
   sim_residual_dates <- data_frame(
     Date = fcst_dates,
     Resid_date = as.Date(NA)
@@ -80,16 +83,16 @@ sample_sequence_similar_days <- function(fcst_dates, resid_dates,
   iD <- fcst_dates[1]
   while (iD <= fcst_dates[length(fcst_dates)]) {
     block_length <- ceiling(runif(1, min_block_length, max_block_length))
-    sample_date <- resid_dates %>% 
-      filter(abs(yday(Date) - yday(iD)) <= 30) %>% 
-      sample_n(1) %>% 
+    sample_date <- resid_dates %>%
+      filter(abs(yday(Date) - yday(iD)) <= date_drift) %>%
+      sample_n(1) %>%
       pull(Date)
-    
+
     idx <- sim_residual_dates$Date %in% seq(iD, iD + block_length - 1, by = "days")
     sim_residual_dates$Resid_date[idx] <- seq(sample_date, sample_date + block_length - 1, by="days")[1:sum(idx)]
-    
+
     iD <- iD + block_length
   }
-  
+
   return(sim_residual_dates)
 }
